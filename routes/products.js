f1a7f0aa81d0831cb743db27ebc0e21a8d25686c
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-03 15:14:00 +0100

    feat: Implement products API with routes for fetching products and genres

---
+++ routes/products.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getGenres, getProducts } from "../controllers/productsControllers.js";
+
+export const productsRouter = express.Router();
+
+productsRouter.get("/", getProducts);
+
+productsRouter.get("/genres", getGenres);
